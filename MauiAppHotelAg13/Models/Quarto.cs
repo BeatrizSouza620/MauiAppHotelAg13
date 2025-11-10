a1a4f8d47cf647be28ab948ada7e3e082c567dc9
BeatrizSouza620 <beatriz.souza620@etec.sp.gov.br>
2025-11-09 22:50:23 -0300

    Add recursos ao app

---
+++ MauiAppHotelAg13/Models/Quarto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiAppHotelAg13.Models
+{
+    public class Quarto
+    {
+        public string Descricao {  get; set; }
+        public double ValorDiariaAdulto { get; set; }
+        public double ValorDiariaCrianca { get; set; }
+    }
+}
